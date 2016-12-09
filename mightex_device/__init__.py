4d929204ab1aae8252942d3279a2bf859821535a
Peter Polidoro <peterpolidoro@gmail.com>
2016-12-09 09:42:43 -0500

    Made changes to work with Python 3.

--- mightex_device/__init__.py
+++ mightex_device/__init__.py
@@ -4,4 +4,4 @@ MightexDevice, MightexDevices, and MightexStage, which contain an instance
 of serial_device2.SerialDevice and adds methods to it to interface to
 Mightex LED controllers.
 '''
-from mightex_device import MightexDevice, MightexDevices, MightexError, find_mightex_device_ports, find_mightex_device_port, __version__
+from .mightex_device import MightexDevice, MightexDevices, MightexError, find_mightex_device_ports, find_mightex_device_port, __version__
